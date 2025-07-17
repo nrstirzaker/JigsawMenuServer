c7fd6fe2f54e147ddfd28f7eb7bd8a964148de27
cjrChuddy <cormacrobertson4@gmail.com>
2025-07-17 10:14:26 +0100

    Adding extra html pages and styling with tailwindcss

--- app.js
+++ app.js
@@ -3,12 +3,8 @@ const path = require("path");
 const port = 3000;
 
 let app = express();
-let publicPath = path.join(__dirname, "public")
 
-
-app.get("/", function (req, res) {
-    res.sendFile(publicPath + "/index.html")
-});
+app.use(express.static("public"));
 
 app.listen(port)
 console.log("Listening on port " + port)
